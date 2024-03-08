905ee1abee388623e8c9cb3ad00ca358cc8a4beb
DungNB <nbdung0000@gmail.com>
2024-03-08 12:18:15 +0700

    oke bro

---
+++ Q3/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Q3.Models;
+
+namespace Q3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult List()
+        {
+            List<Employee> list = new List<Employee>();
+            using (var context = new PE_Fall21B5Context())
+            {
+                list = context.Employees.ToList();
+            }
+            return View(list);
+        }
+        public IActionResult Delete(int Id)
+        {
+            string sql = $"delete from Employee where Id = {Id}"+
+                $"delete from Employee where Id = {Id}";
+
+            using (var context = new PE_Fall21B5Context())
+            {
+                context.Database.ExecuteSqlRaw(sql);
+            }
+            return Redirect("/Employee/List");
+        }
+    }
+}
